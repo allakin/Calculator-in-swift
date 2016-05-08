6e5d04e72234e878d943d3b00751e7027ecba80b
Pavel <19paul91@mail.ru>
2016-05-08 20:19:12 +0300

    add default files

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Павел Анплеенко on 08/05/16.
+//  Copyright © 2016 Павел Анплеенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
